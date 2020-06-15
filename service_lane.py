df5eb2d53a0f5546ec2f9c7cb275d6b2d34d531e
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-06-15 09:18:52 +0530

    Update service_lane.py

--- service_lane.py
+++ service_lane.py
@@ -2,7 +2,7 @@
 # Complete the serviceLane function in the editor below. It should return an array of integers representing the maximum width vehicle that can pass through each segment of the highway described.
 # serviceLane has the following parameter(s):
 # n: an integer denoting the size of the width array
-cases: a two dimensional array of integers where each element is an array of two integers representing starting and ending indices for a segment to consider .
+# cases: a two dimensional array of integers where each element is an array of two integers representing starting and ending indices for a segment to consider .
 
 n, t = [int(x) for x in input().split()]
 width = [int(x) for x in input().split()]
